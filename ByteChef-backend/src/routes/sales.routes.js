93f802554271edfa2aa7286b656dd7de5f137e0a
Rayssa Silva <rayssalemescrz05@outlook.com>
2025-12-29 14:56:14 -0300

    Revert "first commit"
    This reverts commit 83e8bcfa1fc0f8260f2f45793b68dc4b1a081281.

--- ByteChef-backend/src/routes/sales.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { openSale } from "../controllers/sales.controller.js";
-
-const router = Router();
-
-// Abrir nova venda
-router.post("/sales/open", openSale);
-
-export default router;
